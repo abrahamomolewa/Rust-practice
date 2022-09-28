6e87db984f5e7bfdfbffe7250cfe7ff02d03a3e9
abrahamomolewa <abrahamomolewacrib@gmail.com>
2022-09-28 12:45:16 +0100

    first commit to rust-practice repository

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("This is not where you should be; Direction on cargo below:");
+  let mut vec = Vec::new();
+  
+  vec.push("clone this repository");
+  vec.push("type: cargo --bin basics");
+  vec.push("type: cargo --bin expression");
+
+
+
+    while let Some(pop) = myStack.pop() {
+        println!("{}", pop);
+    }
+  
+
+
+
 }
